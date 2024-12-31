f5daaa0bd54e0e753822b051e5b4febfdb78a4bd
CHAMUD12 <chamudgamage@gmail.com>
2024-12-31 15:53:28 +0530

    item delete successful

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import {CustomerProvider} from "./store/CustomerProvider.tsx";
 import {createBrowserRouter, RouterProvider} from "react-router";
 import {Dashboard} from "./pages/Dashboard.tsx";
 import {Add} from "./pages/Add.tsx";
-import {DeleteCustomer} from "./pages/DeleteCustomer.tsx";
+import {Delete} from "./pages/Delete.tsx";
 import {UpdateCustomer} from "./pages/UpdateCustomer.tsx";
 import {RootLayout} from "./components/RootLayout.tsx";
 import {ItemProvider} from "./store/ItemProvider.tsx";
@@ -16,7 +16,7 @@ function App() {
             children : [
                 { path : '', element : <Dashboard/>},
                 { path : '/add', element : <Add/>},
-                { path : '/delete', element : <DeleteCustomer/>},
+                { path : '/delete', element : <Delete/>},
                 { path : '/update', element : <UpdateCustomer/>}
             ]
         },
